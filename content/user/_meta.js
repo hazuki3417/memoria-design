4dabf7fb8546bf766c0bf3c17513f858bddec5ce
Takashi Kudo <hazuki3417@gmail.com>
2026-08-23 17:08:31 +0900

    docs: User設計を追加する

---
+++ content/user/_meta.js
@@ -0,0 +1,10 @@
+export default {
+  "index": "User",
+  "use-cases": "ユースケース",
+  "authentication-authorization": "認証と認可",
+  "data-model": "論理データモデル",
+  "physical-data-model": "物理データモデル",
+  "record-patterns": "レコードパターン",
+  "consistency-transactions": "整合性とTransaction",
+  "design-decisions": "設計判断"
+}
